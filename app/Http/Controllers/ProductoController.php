8aae1501404e88e9bdf4ce0a7dd5dffe73ebdc31
Your Name <you@example.com>
2025-02-22 05:17:37 +0100

    Vista productos renderizada

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
     public function index()
     {
+        $productos  = Producto::all();
         return view('productos.index', compact('productos'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,3 @@ Route::resource('productos', ProductoController::class)->names([
     'update' => 'productos.update',
     'destroy' => 'productos.destroy',
 ]);
-    Route::resource('categorias', CategoriaController::class)->names([
-        'index' => 'categorias.index',
-        'show' => 'categorias.show',
-        'create' => 'categorias.create',
-        'store' => 'categorias.store',
-        'update' => 'categorias.update',
-        'destroy' => 'categorias.destroy',
-    ]);
-    
-    Route::resource('productos', ProductoController::class)->names([
-        'index' => 'productos.index',
-        'show' => 'productos.show',
-        'create' => 'productos.create',
-        'store' => 'productos.store',
-        'update' => 'productos.update',
-        'destroy' => 'productos.destroy',
-    ]);
-    
